c3f52ae0f4abf45f18f9c208b2124af8c1522165
mauricio-jimenez <mauricio.jimenezrivera@gmail.com>
2020-11-28 22:24:33 -0300

    antes y despues en secuencias temporales

--- antes/main.js
+++ antes/main.js
@@ -1,14 +1,15 @@
 
-const colores = ["black", "#500", "#050", "#005"] 
+const colores = ["black", "#500", "#050", "#005"]
+const traduccionColores = ["negro", "rojo", "verde", "azul"]
 
 var puntuacion = 0
 var canvasContext
 var cicloAnimacion
 var estadoAnimacion = 0
 var buttonContexts = []
-var permutaciones = []
-var escala = 0
+var pregunta
 var respuestaCorrecta = 0
+var cableadoBotones = []
 var timer
 
 function inicializacion() {
@@ -18,11 +19,11 @@ function inicializacion() {
 	puntuacion = document.getElementById('puntuacion')
 	canvasContext = document.getElementById('canvas').getContext('2d')
 
-	let canvases = document.querySelectorAll('.boton')
+	let canvases = document.querySelectorAll('.sboton')
 	canvases.forEach( function(c) {
 		c.addEventListener("click", evaluacion)
 		c.width = 60
-		c.height = 20
+		c.height = 60
 		ctx = c.getContext('2d')
 		buttonContexts.push( ctx ) 
 	})
@@ -31,11 +32,20 @@ function inicializacion() {
 }
 
 function reset() {
-	generaPermutaciones()
-	respuestaCorrecta = Math.floor( Math.random() * 6 )
-	cicloAnimacion = permutaciones [ respuestaCorrecta ]
+
+	cicloAnimacion = shuffle([1,2,3])
+	pregunta = Math.floor( Math.random() * 2 ) + 1
+	respuestaCorrecta = cicloAnimacion[ pregunta - 1 ]
+	cableadoBotones = shuffle([1,2,3]) 
 	dibujaBotones()
 	dibujaPantalla()
+	actualizaMensaje()
+}
+
+function actualizaMensaje() {
+	let mensaje = document.getElementById("mensaje")
+	let texto = traduccionColores[ cicloAnimacion [ pregunta ]]
+	mensaje.innerHTML = "¿Qué color aparece antes del " + texto + "?"
 }
 
 function dibujaPantalla() {
@@ -74,29 +84,16 @@ function cambiaEstado() {
 
 function dibujaBotones() {
 	
-	for (let i=0; i<6; i++) {
+	for (let i=0; i<3; i++) {
 		buttonContexts[i].clearRect(0,0,60,20)
-		for (let j=0; j<3; j++) {
-			
-			buttonContexts[i].fillStyle = colores[ permutaciones[i][j] ]
-			//buttonContexts[i].fillRect(j*20, 0, 20, 20)
+		
+		// sera la inversa?
+		buttonContexts[i].fillStyle = colores[ cableadoBotones[i] ]
 
-			buttonContexts[i].beginPath()
-			buttonContexts[i].arc(10 + 20*j, 10, 10, 0, Math.PI * 2)
-			buttonContexts[i].fill()
-		}
+		buttonContexts[i].beginPath()
+		buttonContexts[i].arc(30, 30, 30, 0, Math.PI * 2)
+		buttonContexts[i].fill()
 	}		
-}
-
-function generaPermutaciones() {
-	
-	let opciones = [ [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1] ] 
-	let orden = shuffle( [0,1,2,3,4,5] )
-	permutaciones = []
-
-	for (let i=0; i<6; i++) {
-		permutaciones.push( opciones[ orden[i] ] )
-	}
 }
 
 
@@ -104,7 +101,8 @@ function evaluacion(e) {
 	
 	let nuevaPuntuacion;
 
-	respuesta = e.currentTarget.getAttribute("value")
+	irespuesta = e.currentTarget.getAttribute("value")
+	respuesta = cableadoBotones[ irespuesta ]
 
 	if (respuesta == respuestaCorrecta) {		
 		
@@ -116,6 +114,7 @@ function evaluacion(e) {
 			document.getElementById("mensaje").innerHTML = "Muy bien! Lo lograste!";
 			document.getElementById("fanfare").play();
 			return
+
 		}
 	
 		document.getElementById("buena").play();
