5e2cbb20d9eb00fe423df07fbcf79d2f1b1f39fd
mauricio-jimenez <mauricio.jimenezrivera@gmail.com>
2020-12-19 19:10:26 -0300

    comparacion de cantidades

---
+++ donde_hay_mas/main.js
@@ -0,0 +1,73 @@
+var puntuacion;
+var opciones = ["porotos", "maiz", "leche", "damascos", "crayones", "botones", "avena", "arroz", "algodon"];
+var respuestaCorrecta;
+var imagenes;
+
+function inicializacion() {
+    
+	puntuacion = document.getElementById('puntuacion');
+
+	imagenes = document.querySelectorAll("img");	
+	imagenes.forEach( function(b) {
+		// eventlistener
+		b.addEventListener("click", evaluacion );
+	})
+}
+
+function reset() {
+
+	respuestaCorrecta = Math.floor( Math.random() * 2 )
+	let i = Math.floor( Math.random() * 9 )
+	cargaImagenes(i)
+	cargaMensaje(i)
+}
+
+function cargaImagenes(i) {
+	if (respuestaCorrecta) {
+		imagenes[0].src = "../imagenes/compara/" + opciones[i] + "-" + "poco" + ".jpg"
+		imagenes[1].src = "../imagenes/compara/" + opciones[i] + "-" + "mucho" + ".jpg"
+	} else {
+		imagenes[0].src = "../imagenes/compara/" + opciones[i] + "-" + "mucho" + ".jpg"
+		imagenes[1].src = "../imagenes/compara/" + opciones[i] + "-" + "poco" + ".jpg"
+	}
+}
+
+function cargaMensaje(i) {
+	document.getElementById("mensaje").innerHTML = "¿Dónde hay más " + opciones[i] + "?"
+}
+
+function evaluacion(e) {
+	
+	let nuevaPuntuacion
+
+	respuesta = e.currentTarget.getAttribute("value")		
+
+	if (respuesta == respuestaCorrecta) {		
+		
+		nuevaPuntuacion = parseInt(puntuacion.innerHTML) + 1;
+		puntuacion.innerHTML = nuevaPuntuacion; 		
+
+		if ( nuevaPuntuacion == 20) {
+
+			document.getElementById("mensaje").innerHTML = "Muy bien! Lo lograste!";
+			document.getElementById("fanfare").play();
+			return
+		}
+		
+		document.getElementById("buena").play();
+
+	}
+	else {
+		// quizas poner un sonido...
+		nuevaPuntuacion = parseInt(puntuacion.innerHTML) - 1;
+		if (nuevaPuntuacion >= 0) {
+			puntuacion.innerHTML = nuevaPuntuacion; 		
+		}
+	}
+	
+	reset();
+}
+	
+inicializacion()
+reset()
+
